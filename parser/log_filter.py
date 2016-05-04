ad7a8c43992d816372475925f1e7d353ee53b6d8
AdamPiech <adampiech7@op.pl>
2016-05-04 11:45:38 +0200

    Skończona funkcjonalnosć bez wyjątków.

--- parser/log_filter.py
+++ parser/log_filter.py
@@ -1,15 +1,18 @@
 import sys
+import re
+import time
 from os.path import os
 
+# from parser.exceptions.InvalidPathException import InvalidPathException
+
 log_file_lines = []
-log_file_path = "../../RXOTG-513_trx0_log_fault_1.txt"
-parse_log_file_path = "../../parse_log"
+parse_log_file_path = "../../parse_" # TEMPONARY
 settings_file = "../settings/settings"
 ignore_file = "../settings/ignore"
 
 keywords = set()
 ignore_words = set()
-write_mode = 'w'
+write_mode = 'a'
 read_mode = 'r'
 
 def read_options_file(path, mode, collection):
@@ -17,31 +20,43 @@ def read_options_file(path, mode, collection):
         for settings in read_settings:
             collection.add(settings.replace('\n', ''))
 
-def read_log(path, mode, collection):
+def read_log(path, mode):
     count_lines = 0
+    log_collection = []
     with open(path, mode) as read_log_file:
         for line in read_log_file:
             count_lines = count_lines + 1
             if any(key in line.lower() for key in keywords) and not (any(ignore in line for ignore in ignore_words)):
-                collection.append("Line number: " + str(count_lines) + "   Message :   " + line)
+                log_collection.append("Line number: " + str(count_lines) + "   Message :   " + line)
+    return log_collection
 
 def write_parse_log(path, mode, collection, description):
     with open(path, mode) as write_log_file:
-        write_log_file.write("\n" * 3 + "#" * 25 + "   LOG FROM: " + description + "   " + "#" * 25 + "\n" * 3)
+        write_log_file.write("\n" * 2 + "#" * 15 + "   LOG FROM: " + description + "   CURRENT SERVER TIME: " + time.strftime("%y/%m/%d %H:%M:%S") + "   " + "#" * 15 + "\n" * 3)
         write_log_file.writelines(collection)
 
-def find_file(path, extension):
-    print path
-    print extension
-    file_list = [path + "/" + file for file in os.listdir(path)] # if splitext(file)[1].lower() == "." + "extension"]
-    print file_list
+def find_file(path, regexp, extension = "txt"):
+    if os.path.isdir(path):
+        return [path + "/" + file for file in os.listdir(path) if re.search(".*(" + regexp + "){1}.*" + extension + "{1}", file)]
+    # else:
+    #     raise InvalidPathException, "ERROR: Invalid path!"
 
 read_options_file(settings_file, read_mode, keywords)
 read_options_file(ignore_file, read_mode, ignore_words)
-read_log(log_file_path, read_mode, log_file_lines)
-write_parse_log(parse_log_file_path + ".log", write_mode, log_file_lines, log_file_path)
 
-find_file("/afs/ericpol.int/home/p/6/p660/home/workspace/Python_Git/PythonLogFilter/parser","") # zmianiec na argument
-
-if sys.argv.__len__() > 2:
-    find_file(str(sys.argv[1]),str(sys.argv[2]))
+if len(sys.argv) == 4:
+    for file in find_file(sys.argv[1], sys.argv[2], sys.argv[3]):
+        # try:
+        log_file_lines = read_log(file, read_mode)
+        write_parse_log(parse_log_file_path + sys.argv[2] + ".log", write_mode, log_file_lines, file)
+        # except InvalidPathException, err:
+        #     print err
+elif len(sys.argv) == 3:
+    for file in find_file(sys.argv[1], sys.argv[2]):
+        # try:
+        log_file_lines = read_log(file, read_mode)
+        write_parse_log(parse_log_file_path + sys.argv[2] + ".log", write_mode, log_file_lines, file)
+        # except InvalidPathException, err:
+        #     print err
+else:
+    print "ERROR: Invalid number of arguments!"
