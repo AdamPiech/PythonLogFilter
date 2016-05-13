b732d2f19572eb1ed5693cb29b5b2958f742f74e
AdamPiech <adampiech7@op.pl>
2016-05-13 10:53:03 +0200

    Dodano komentarze.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 Parametry:
 
--p --path [path]            - Ścieżka do pliku (można podać "." dla katalogu w którym znajduje się plik, lub ".." dla wycofania do katalogu wyżej);
--r --regexp [regexp]        - Wyrażenie regularne, według którego wyszukujemy nazwy plików. Jeżeli chcemy wyszukać wszystkie podajemy np. ".*". Domyślnie pobiera wybrane patterny;
--e --extension [extension]  - Rozszerzenie pliku (domyślnie "txt").
+
+
+-p, --path        - Ścieżka do pliku (można podać "." dla katalogu w którym znajduje się plik, lub ".." dla wycofania do katalogu wyżej);
+
+-r, --regexp      - Wyrażenie regularne, według którego wyszukujemy nazwy plików. Jeżeli chcemy wyszukać wszystkie podajemy np. ".*". Domyślnie pobiera wybrane patterny;
+
+-e, --extension   - Rozszerzenie pliku (domyślnie "txt").
 
 
 
--- log_filter.py
+++ log_filter.py
@@ -5,23 +5,34 @@ from os.path import os
 import getopt
 
 settings_file = os.path.join('settings', 'extract')
+# path to extract patterns on Jenkins
+# settings_file = os.path.join(os.sep, 'proj', 'bscautodata', 'bts_ci', 'rbslog_parser', 'settings', 'extract')
 ignore_file = os.path.join('settings', 'ignore')
+# path to ignore patterns on Jenkins
+# ignore_file = os.path.join(os.sep, 'proj', 'bscautodata', 'bts_ci', 'rbslog_parser', 'settings', 'ignore')
+
+# collection of all extracted lines from log
 log_file_lines = []
+# collection of regular expressions to extract from log
 keywords = set()
+# collection of words or lines to ignore from parsed line
 ignore_words = set()
 write_mode = 'a'
 read_mode = 'r'
 
+# default parser settings when no parameters passed
 path_arg = '..'
 regexp_arg = ('dx', 'trx', 're', 'ec')
 extension_arg = 'txt'
 
+# getting arguments from command line
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'p:r:e:', ['path=', 'regexp=', 'extension='])
 except getopt.GetoptError:
     print "ERROR: Invalid arguments!"
     sys.exit(2)
 
+# if argument is passed, it is set to the variable
 for opt, arg in opts:
     if opt in ('-p', '--path'):
         path_arg = arg
@@ -33,15 +44,21 @@ for opt, arg in opts:
     else:
         sys.exit(2)
 
+# path to parsed result
 parse_log_file_path = path_arg + os.sep + time.strftime("%d-%m-%y_%H-%M-%S") + '_parse_'
 
 
+# function used to read settings from keywords and ignore_words collections
 def read_options_file(path, mode, collection):
     with open(path, mode) as read_settings:
         for settings in read_settings:
             collection.add(settings.replace('\n', ''))
 
 
+# Function used to read specified logs. It walks line by line through log file, checks if line contains pattern from
+# keywords and then checks if there is pattern from ignored_words. In case it contains pattern from ignored_words,
+# the line is not appended to log_collection, otherwise it is appended to log_collection.
+# There is also a message about number of line, where the pattern was.
 def read_log(path, mode):
     count_lines = 0
     log_collection = []
@@ -53,6 +70,7 @@ def read_log(path, mode):
     return log_collection
 
 
+# This function is used to write a resulting collection (from read_log) with description about log file.
 def write_parse_log(path, mode, collection, description):
     with open(path, mode) as write_log_file:
         write_log_file.write("\n" * 2 + "#" * 15 + "   TYPE OF LOG: " + type_of_log(description) + "   LOG FROM: " + description +
@@ -60,6 +78,7 @@ def write_parse_log(path, mode, collection, description):
         write_log_file.writelines(collection)
 
 
+# This function checks the type of log. It gets a filename of current log file and returns a type of log.
 def type_of_log(filename):
     if isinstance(regexp_arg, tuple):
         for log_type in regexp_arg:
@@ -70,6 +89,8 @@ def type_of_log(filename):
             return regexp_arg
 
 
+# This function is used to get all files from selected directory, which contains specified regexp and/or extension in
+# its name. It retuns a list of path to file.
 def find_file(path = '.', regexp = '.*', extension = 'txt'):
     if os.path.isdir(path):
         if isinstance(regexp_arg, tuple):
@@ -85,10 +106,13 @@ def find_file(path = '.', regexp = '.*', extension = 'txt'):
         print "ERROR: Invalid path!"
         sys.exit(2)
 
-
+# Read settings
 read_options_file(settings_file, read_mode, keywords)
 read_options_file(ignore_file, read_mode, ignore_words)
 
+# This loop is used to parse selected log files. Name of the resulting parsed file is created by a name
+# of pattern which was used to choose files to parse. The resulting log file is written in directory
+# where the logs files were found.
 for file in find_file(path_arg, regexp_arg, extension_arg):
     log_file_lines = read_log(file, read_mode)
     write_parse_log(parse_log_file_path + ("all" if isinstance(regexp_arg, tuple) else regexp_arg) + ".log", write_mode, log_file_lines, file)
